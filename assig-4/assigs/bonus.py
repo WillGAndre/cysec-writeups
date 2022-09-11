d489d28680c209bc979fec938806968642902210
WillGAndre <xunga@mail.com>
2022-09-11 20:39:10 +0100

    assig-4

---
+++ assig-4/assigs/bonus.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+muHa = "bAF0rTG5AY2PMO86AtOzvcqDxibbRyfIAJ38rzvOxZVsSp6!WBi7QvOSHSd3Lz4BtpSePA6jn9Kdcqfrodr4YjP9fdgkGOBjD!N6CLOvEkyvwTx8DAlFSyFHdbOMfeWZpMEyxRbYy1hgat2EOqZLoGgemWhBCynRj2tpijuTacdMXZYNctDx8PbSI8FHS5fNdQPVvVWiYnQouUxEWHNYVpNkmyyBUTB4PzvWhLulUVTy4gulGhzgRLVydgPD3zbu3GlwwWjD2v7itpPBBVz3skHqmmx6MAOskoc7oP90fRq1BW2cA01f1jAST3UQdryVT8x3iBzYfRToFnpKMRVfmJtQRNlkHv0av1onhjSpLUChop5rwPxXTUWEUHLrrIwIxUO1mAr2TpKAjr1rEycue4f1uL7QQ2MPBpzMxFDe6qjU0ytEkWjRFZahlO20CJDsMl4VcVjqo4gyOCaONTQwZuRqClv9yDZQIodTqZj0NZXK5fF5tGn8RP$AU45u#TR?as3"
+bb = b'EAeeFGJDUZEeaishu4qaxswNUmKcmTDn1HjrKYN8U7Y='
+b = b'gAAAAABjDLCIjp7gZPdLEynEO0XaCNusDgM5jjUkKQ7hoXLGIQq4t5UoQw9WmsgAup3XlBkqkwWhgsQM7qYuj3piy0xpRfQnh4F6Pa6XOkXimrH9gjtQnphSnri_3q8Y6oshPIcz9rbgl1CVjCpEGEi3DPRS5Oe-CstC_CNQG-pDFmU255PeksQdMWNI2v9e3Ugy22hwB8ndgW6H3jNTZ89ru5DH-MUKLFMwcYEKQtQhC3XlAwJaOXwU_WZ5X9TMbV1ODBChm-taN8T2V0pU8hx9Yji5brVHfgUfXGcAdGZKGJItMpixYZ0ISvnBvHB04bWNwlf1eBMmG_3Jpl33SerrCCIe4OfOPpB2ktRbhDceppl6mlnwbfo75ZZAMNbqP3RRK2-eWjFEKY47GTf2pN6KMKW6V2s3nK9lZpP4FLPVJ16OsuPLd8Q6NrcKwt2o5sYPA54M3fb22-4U6hQhkmVgJ_m24SHRCw=='
+
+f = Fernet(bb)
+print(f.decrypt(b))
+
+# dyrxjtyjxfgn = input(muHa[501]+muHa[503]+muHa[55]+muHa[57]+muHa[511]+' ')
+
+print(muHa[100]+muHa[200]+muHa[300]+muHa[400]+muHa[500]+muHa[250]+muHa[125]+muHa[75]+muHa[50]+muHa[25])
+print(muHa[401]+muHa[403]+muHa[45]+muHa[47])
+print(muHa[201]+muHa[203]+muHa[65]+muHa[67]+muHa[97])
+
+# if dyrxjtyjxfgn == muHa[100]+muHa[200]+muHa[300]+muHa[400]+muHa[500]+muHa[250]+muHa[125]+muHa[75]+muHa[50]+muHa[25]:
+#     print(muHa[401]+muHa[403]+muHa[45]+muHa[47])
+# else:
+#     print(muHa[201]+muHa[203]+muHa[65]+muHa[67]+muHa[97])
